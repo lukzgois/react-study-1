d3d750d4ff7309902f5092f65b2513a6340d3977
Lucas Gois <lucaspgois@gmail.com>
2023-11-10 19:11:16 -0300

    Creating the footer component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import Banner from './components/Banner/Banner'
+import Footer from './components/Footer';
 import Form from './components/Form'
 import Team from './components/Team'
 
@@ -40,6 +41,8 @@ function App() {
           />
         )
       })}
+
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer/index.js
@@ -0,0 +1,31 @@
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <footer class="footer">
+      <div class="footer-social">
+        <a href="#">
+          <img src='/images/fb.png' />
+        </a>
+
+        <a href="#">
+          <img src='/images/tw.png' />
+        </a>
+
+        <a href="#">
+          <img src='/images/ig.png' />
+        </a>
+      </div>
+
+      <div>
+        <img src='/images/logo.png' />
+      </div>
+
+      <div>
+        Desenvolvido por Alura.
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
